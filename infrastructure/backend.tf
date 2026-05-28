ebc42e332739ce49bbf87bbbafb2a5270c8cbfdd
03-Andrew <andreinadela052@gmail.com>
2026-05-28 19:01:52 +0800

    chore: update Terraform version to 1.10.5 and set required version in configuration

--- infrastructure/backend.tf
+++ infrastructure/backend.tf
@@ -1,9 +1,11 @@
 terraform {
+  required_version = ">= 1.10.0"
+
   backend "s3" {
-    bucket         = "runbot-terraform-state"
-    key            = "activitybot/terraform.tfstate"
-    region         = "ap-southeast-1"
-    encrypt        = true
-    use_lockfile   = true
+    bucket       = "runbot-terraform-state"
+    key          = "activitybot/terraform.tfstate"
+    region       = "ap-southeast-1"
+    encrypt      = true
+    use_lockfile = true
   }
 }

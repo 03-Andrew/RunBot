612f0e126a1e31c1d2b1e95aadaab88ebb6430a3
03-Andrew <andreinadela052@gmail.com>
2026-06-04 20:43:23 +0800

    feat: remove AWS credentials variables from infrastructure configuration

--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -40,12 +40,3 @@ variable "gemini_api_key" {
   sensitive = true
 }
 
-variable "aws_access_key_id" {
-  type      = string
-  sensitive = true
-}
-
-variable "aws_secret_key" {
-  type      = string
-  sensitive = true
-}
